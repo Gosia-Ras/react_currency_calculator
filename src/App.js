a0a35f85908160c3cc0ed0d72fa238079fe5f38d
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-06-18 09:56:02 +0200

    form added to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Container from "./Container";
 import Header from "./Header";
+import Paragraph from "./Paragraph";
+import Form from "./Form";
 
 function App() {
   return (
     <Container>
        <Header title="Currency Calculator" />
+       <Paragraph title="Currency rates 18.06.21 from the site of Polish National Bank"/>
+       <Form />
     </Container>
   );
 }
---
+++ src/Form/index.js
@@ -0,0 +1,41 @@
+import "./style.css";
+
+const Form = () => (
+    <form className="form">
+            <fieldset className="form__fieldset">
+                <p>
+                    <label>
+                        <span className="form__labelText">Amount in PLN*:</span>
+                        <input 
+                            step="any" 
+                            min="1" 
+                            className="form__field"
+                            placeholder="Enter the amount in PLN" 
+                            type="number" 
+                            required />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">Currency:</span>
+                        <select className="form__field">
+                            <option value="">Pick the currency</option>
+                            <option value="EUR">EUR</option>
+                            <option value="CHF">CHF</option>
+                            <option value="USD">USD</option>
+                            <option value="GBP">GBP</option>
+                            <option value="RUB">RUB</option>
+                        </select>
+                    </label>
+                </p>
+
+                <p className="form__paragraph js-form__paragraph--result"></p>
+            </fieldset>
+            <p>
+                <button className="form__button" type="submit">Calculate</button>
+                <button className="form__button" type="reset">Reset</button>
+            </p>
+        </form>
+);
+
+export default Form;
