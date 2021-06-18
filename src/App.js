74a2923db67480472455a84277b41500b13d486a
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-06-18 13:43:45 +0200

    Paragraph removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React from "react";
 import Container from "./Container";
 import Header from "./Header";
-import Paragraph from "./Paragraph";
 import Form from "./Form";
 import Footer from "./Footer";
 import Result from "./Result";
@@ -9,12 +9,11 @@ import Buttons from "./Buttons";
 function App() {
   return (
     <Container>
-       <Header title="Currency Calculator" />
-       <Paragraph title="Currency rates 18.06.21 from the site of Polish National Bank"/>
-       <Form />
-       <Buttons />
-       <Result />
-       <Footer title="Copyright 2021 by Malgorzata Ras. All rights reserved." />
+      <Header title="Currency Calculator" />
+      <Form />
+      <Buttons />
+      <Result />
+      <Footer title="Copyright 2021 by Malgorzata Ras. All rights reserved." />
     </Container>
   );
 }
--- src/Paragraph/index.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-const Paragraph = ({ title }) => (
-    <p className="paragraph">{title}</p>
-);
-
-export default Paragraph;
