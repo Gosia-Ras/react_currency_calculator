cd62580b0dbaf435bbd2415599dd296f7aeba79c
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-06-18 10:02:08 +0200

    footer added to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Container from "./Container";
 import Header from "./Header";
 import Paragraph from "./Paragraph";
 import Form from "./Form";
+import Footer from "./Footer";
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
        <Header title="Currency Calculator" />
        <Paragraph title="Currency rates 18.06.21 from the site of Polish National Bank"/>
        <Form />
+       <Footer title="Copyright 2021 by Malgorzata Ras. All rights reserved." />
     </Container>
   );
 }
