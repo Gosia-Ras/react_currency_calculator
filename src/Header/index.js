aa4e98d87a452e2178672513065a66d83a033c59
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-08-05 16:33:48 +0200

    link to currency rates added

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,9 +1,12 @@
 import "./style.css";
 
 const Header = ({ title }) => (
-    <header className="header">
-      <h1>{title}</h1>
-    </header>
-  );
-  
-  export default Header;
+  <header className="header">
+    <h1>{title}</h1>
+    <a className="header_link" href="https://www.nbp.pl/home.aspx?f=/kursy/kursya.html">
+      Polish National Bank currency rates on 05.08.2021
+    </a>
+  </header>
+);
+
+export default Header;
