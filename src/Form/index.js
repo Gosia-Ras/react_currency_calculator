e766d5682662804b186fda59c3a51902c719091b
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-08-17 16:24:55 +0200

    code cleaned

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,6 @@ import "./style.css";
 import { Result } from "./Result";
 import { currencies } from "../currencies";
 
-
 export const Form = () => {
   const [currency, setCurrency] = useState(currencies[0].short);
   const [amount, setAmount] = useState("");
@@ -11,72 +10,69 @@ export const Form = () => {
   const [result, setResult] = useState();
 
   const calculateResult = (currency, amount) => {
-    const rate = currencies
-    .find(({ short }) => short === currency)
-    .rate;
+    const rate = currencies.find(({ short }) => short === currency).rate;
 
     setResult({
       sourceAmount: +amount,
       targetAmount: amount / rate,
       currency,
     });
-
-  }
+  };
   const onSubmit = (event) => {
     event.preventDefault();
     calculateResult(currency, amount);
   };
 
-  const onClickReset = () => {
+  const onClickReset = (event) => {
+    event.preventDefault();
     setAmount("");
-    setResult("");
-};
-
+    setResult();
+  };
 
   return (
     <form className="form" onSubmit={onSubmit}>
       <fieldset className="form__fieldset">
-          <div className="flex-container">
-          <p>
-            <label>
-              <span className="form__labelText flex-items">PLN*</span>
-              <input
-                value={amount}
-                onChange={({ target }) => setAmount(target.value)}
-                step="any"
-                min="1"
-                max="1000000000"
-                className="form__field flex-items"
-                placeholder="Enter the amount in PLN"
-                type="number"
-                required
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <span className="form__labelText flex-items">Currency</span>
-              <select
-                className="form__field flex-items"
-                value={currency}
-                onChange={({ target }) => setCurrency(target.value)}
-              >
-                {currencies.map((currency) => (
-                  <option key={currency.short} value={currency.short}>
-                    {currency.short}
-                  </option>
-                ))}
-              </select>
-            </label>
-          </p>
-          </div>
+        <p>
+          <label className="form__label">
+            <p>Amount in PLN*:</p>
+            <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              step="any"
+              min="1"
+              max="1000000000"
+              className="form__field"
+              placeholder="Enter the amount in PLN"
+              type="number"
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label className="form__label">
+            <p>Currency</p>
+            <select
+              className="form__field"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.short} value={currency.short}>
+                  {currency.short}
+                </option>
+              ))}
+            </select>
+          </label>
+        </p>
       </fieldset>
       <div className="flexContainer">
-      <button className="form__button" type="submit">
-        Calculate
-      </button>
-      <Result result={result} />
-      <button type="reset" className="form_button" onClick={onClickReset}>Clear form</button>
+        <Result result={result} />
+        <button className="form__button" type="submit">
+          Calculate
+        </button>
+        <button type="reset" className="form__button" onClick={onClickReset}>
+          Clear form
+        </button>
       </div>
     </form>
   );
