050e6f16b3007eb058760b1d83b3125b1df69110
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-08-25 12:23:57 +0200

    redundant code removed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Header from "./Header";
 import { Form } from "./Form";
 import Footer from "./Footer";
 
-function App(result, calculateResult) {
+function App () {
 
   return (
     <Container>
       <Header title="Currency Calculator" />
-      <Form result={result} calculateResult={calculateResult} />
+      <Form />
       <Footer title="Copyright 2021 by Malgorzata Ras. All rights reserved." />
     </Container>
   );
