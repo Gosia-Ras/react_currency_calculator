f90935d63c119f25c56d292e5199e8e7202aa88c
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-08-25 17:25:22 +0200

    added clock to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Container from "./Container";
 import Header from "./Header";
 import { Form } from "./Form";
 import Footer from "./Footer";
+import Clock from "./Clock";
+
 
 function App () {
 
   return (
     <Container>
       <Header title="Currency Calculator" link="Polish National Bank currency rates on 17.08.2021" />
+      <Clock />
       <Form />
       <Footer title="Copyright 2021 by Malgorzata Ras. All rights reserved." />
     </Container>
