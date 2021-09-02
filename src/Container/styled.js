dcc2cb7381c8faecfc5e1f99ef771d2139b734fd
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-09-02 11:36:08 +0200

    styled components added to main

---
+++ src/Container/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.main`
+  background-color: whitesmoke;
+  border-radius: 25px;
+  box-shadow: 10px 10px 13px -7px #777576;
+  max-width: 480px;
+  padding: 15px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
