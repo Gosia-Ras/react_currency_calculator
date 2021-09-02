663fd4880b74c02c2ff4693723a4ef6c3f5b2209
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-09-02 12:10:59 +0200

    GlobalStyle created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import img from "./background-calculator.png"
+
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+     }
+  
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+  
+  #root {
+    background-size: cover;
+    background-image: url(${img});
+    background-position: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+  }`;
