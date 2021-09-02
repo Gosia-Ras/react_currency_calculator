5d815b0e9303e5f5cc0a5df63ae0631073d6f08d
Malgorzata Ras <ras.malgorzata@gmail.com>
2021-09-02 11:38:10 +0200

    styled components added to header

---
+++ src/Header/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  text-align: center;
+  padding: 10px;
+  font-family: "Lato", sans-serif;
+  font-weight: bold;
+  margin: 5px;
+  border: 1px solid black;
+  box-shadow: 5px 5px 12px -9px #000000;
+`;
