775f3a4cdf639108e23e2e8b30dcafe97eacc5d2
Gosia Ras <ras.malgorzata@gmail.com>
2021-11-04 11:49:13 +0100

    border removed from header

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -6,6 +6,4 @@ export const StyledHeader = styled.header`
   font-family: "Lato", sans-serif;
   font-weight: bold;
   margin: 5px;
-  border: 1px solid black;
-  box-shadow: 5px 5px 12px -9px #000000;
 `;
