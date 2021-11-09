2a131f1160e212332b94e53bb005e2179d9854e6
Gosia-Ras <ras.malgorzata@gmail.com>
2021-11-09 16:15:31 +0100

    container height and margin changed

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -5,7 +5,8 @@ export const StyledContainer = styled.div`
   border-radius: 25px;
   box-shadow: 10px 10px 13px -7px #777576;
   padding: 15px;
-  margin: 0 auto;
+  max-height: 100vh;
+  margin-top: 40px;
 
   @media (max-width: 768px) {
     width: 100%;
