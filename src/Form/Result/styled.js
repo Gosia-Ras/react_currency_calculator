c59755ff1c5d4687f534ad27469e7ac29a9d4cf4
Gosia-Ras <ras.malgorzata@gmail.com>
2021-11-12 19:57:27 +0100

    media query added to Result

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -4,4 +4,14 @@ export const StyledResult = styled.p`
   height: 40px;
   text-align: center;
   margin-top: 50px;
+
+  @media (max-width: 768px) {
+    height: 40px;
+    text-align: center;
+  }
+
+  @media (max-width: 320px) {
+    height: 40px;
+    text-align: center;
+  }
 `;
