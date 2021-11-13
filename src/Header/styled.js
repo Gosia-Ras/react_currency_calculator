e70ccd2d6649994acc7292d85e2916380863d816
Gosia-Ras <ras.malgorzata@gmail.com>
2021-11-13 16:37:29 +0100

    font removed

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
   text-align: center;
   padding: 10px;
-  font-family: "Lato", sans-serif;
   font-weight: bold;
   margin: 5px;
 `;
