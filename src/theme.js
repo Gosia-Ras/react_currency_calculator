54703dc90010f064abf7727077156e6a117c9a0d
Gosia-Ras <ras.malgorzata@gmail.com>
2021-11-14 19:11:55 +0100

    theme.js designed

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,31 @@
-
-const colorNames = {};
+const colorNames = {
+  oxfordBlue: "#343F51",
+  whitesmoke: "#f5f5f5",
+  grey: "#ADADAD",
+  hover: "#3d4f66",
+  lightGrey: "#6E747C",
+  boxGrey: "#dadada",
+  arrowGrey: "ccc",
+  red: "#A51818"
+};
 
 export const theme = {
-  color: {},
-  backgroundColor: {},
-  button: {},
-  image: {},
-  accent: {},
-  breakpoint: {s},
+  color: {
+    primary: colorNames.whitesmoke,
+    secondary: colorNames.oxfordBlue,
+    tertiary: colorNames.grey,
+  },
+  button: {
+    hover: colorNames.hover,
+  },
+  accent: {
+    clock: colorNames.lightGrey,
+    selectArrow: colorNames.arrowGrey,
+    shadowSelect: colorNames.boxGrey,
+    failure: colorNames.red,
+  },
+  breakpoint: {
+    mobile: "320px",
+    tablet: "768px",
+  },
 };
