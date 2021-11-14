31f4bf5957ca2a60a4a4e24472b5fdd0ca43ec6d
Gosia-Ras <ras.malgorzata@gmail.com>
2021-11-14 19:12:58 +0100

    theme added to container

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  background-color: whitesmoke;
+  background-color: ${({ theme}) => theme.color.primary};
   border-radius: 25px;
-  box-shadow: 10px 10px 13px -7px #777576;
+  box-shadow: 10px 10px 13px -7px ${({ theme}) => theme.color.tertiary};
   padding: 15px;
   max-height: 100vh;
   margin-top: 40px;
