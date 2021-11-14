b17d92a16aea6f2759ebda163e0bc5dc085ec940
Gosia-Ras <ras.malgorzata@gmail.com>
2021-11-14 19:13:04 +0100

    theme added to footer

--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   text-align: center;
-  background-color: whitesmoke;
+  background-color: ${({ theme}) => theme.color.primary};
   padding: 10px;
   font-size: small;
   font-style: italic;
   margin-top: 30px;
-  color: #5B626B;
+  color: ${({ theme }) => theme.color.tertiary};
 
   @media (max-width: 320px) {
     background-color: transparent;
-  };
+  } ;
 `;
