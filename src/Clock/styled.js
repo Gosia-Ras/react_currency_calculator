1fcfda0a2d61d471d5eaaeeefb8f729c11327819
Gosia-Ras <ras.malgorzata@gmail.com>
2021-11-14 19:12:30 +0100

    theme added to clock

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const StyledClock = styled.p`
   font-family: monospace;
   text-align: right;
-  color: gray;
+  color: ${({ theme }) => theme.accent.clock};
   margin-right: 6px;
 `;
