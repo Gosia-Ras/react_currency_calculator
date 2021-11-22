5af7b28e43b6020fc3dbdc4441f818efb8ccaa0d
Gosia Ras <ras.malgorzata@gmail.com>
2021-11-22 16:09:45 +0100

    height removed

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -5,11 +5,5 @@ export const StyledContainer = styled.div`
   border-radius: 25px;
   box-shadow: 10px 10px 13px -7px ${({ theme}) => theme.color.tertiary};
   padding: 15px;
-  max-height: 100vh;
   margin-top: 40px;
-
-  @media (max-width: 320px) {
-    max-height: fit-content;
-    background-color: #f5f5f5d6;
-  };
 `
